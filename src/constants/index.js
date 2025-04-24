db88fddaf3a922e1ac228337bc4e9d624b26f394
Yash Parmar <0808io201056.ies@ipsacademy.org>
2025-04-24 12:06:20 +0530

    update HERO_CONTENT and EXPERIENCES to reflect accurate experience details

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,14 +4,14 @@ import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 import project5 from "../assets/projects/project-5.png";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React as well as back-end technologies like Node.js, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With more than 2 years of hands-on experience, I have honed my skills in front-end technologies like React as well as back-end technologies like Node.js, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including React, Node.js, MySQL and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "Dec 2024 - Present",
-    role: "UI / UX Developer",
+    year: "Feb 2024 - Present",
+    role: "Software Developer",
     company: "Nerds & Geeks Pvt. Ltd.",
     link: "https://nerdsandgeeks.in/",
     description: `Led a team of 4 developers to build a client-facing MERN application, improving user engagement by 35%. 
